72c06dc10910a70ff8aabb66e95c5987bc8419ac
Abdulrahman Al Abri <abdulrahman.alabri@student.manchester.ac.uk>
2024-09-30 23:46:20 +0100

    Added Task 4

--- Lab_01.py
+++ Lab_01.py
@@ -9,5 +9,16 @@ greenLight = 0
 printState()
 
 print(3 + 5)
-
 print("3" + "5")
+
+x = 10 # Intger
+y = 20.0 # Float
+z = 1j # Complex Number
+
+print(type(x), type(y), type(z), sep='\n')
+
+x = int(1) # is 1 int
+y = int(2.8) # is 2 int
+z = int("3") # is 3 int
+
+print(type(x), type(y), type(z), sep='\n')
